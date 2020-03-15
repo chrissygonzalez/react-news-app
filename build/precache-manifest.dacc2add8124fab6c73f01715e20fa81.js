a550d6997075eb2ebcd8ace4e97c83321ed90c20
chrissygonzalez <chrissygonzalez@gmail.com>
2020-03-14 22:02:11 -0400

    remove package-lock.json etc

---
+++ build/precache-manifest.dacc2add8124fab6c73f01715e20fa81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae7714b2a93b58503bc8669030d02f9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed988eaef6f57180b38e",
+    "url": "/static/css/main.a074eca5.chunk.css"
+  },
+  {
+    "revision": "8f71747b585301619cff",
+    "url": "/static/js/2.20e6845d.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.20e6845d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed988eaef6f57180b38e",
+    "url": "/static/js/main.f1c78def.chunk.js"
+  },
+  {
+    "revision": "373bc86963ec87b1e452",
+    "url": "/static/js/runtime-main.677e7dbe.js"
+  }
+]);
